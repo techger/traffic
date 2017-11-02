5fd934ea22b67cd89da0997919436853e99d2ed9
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-11-02 21:46:17 +0000

    follower follows added

--- src/UserCard.js
+++ src/UserCard.js
@@ -39,10 +39,12 @@ const UserCard = ({ userDetail }) => {
                         <svg aria-hidden="true" className="octicon octicon-link" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path fillRule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>
                         <a href={blog}>{blog}</a>
                     </div>
-                    {/* <div className="UserCard-follow">
-                        <span>Followers: <a href={followers_url}>{followers}</a></span>
-                        <span>Following: <a href={following_url}>{following}</a></span>
-                    </div> */}
+                    {
+                        <div className="UserCard-follow">
+                            <span>Followers: <a href={followers_url}>{followers}</a></span>
+                            <span>Following: <a href={following_url}>{following}</a></span>
+                        </div>
+                    }
                 </section>
             </main>
         </div>
@@ -55,7 +57,7 @@ UserCard.propTypes = {
 const Title = ({ name, login, url }) => {
     return (
         <div className="UserCard-title">
-            <span>Visitor Grahp for {name} (<a href={url}>{login}</a>)</span>
+            <span>Visitor Graph for {name} (<a href={url}>{login}</a>)</span>
         </div>
     );
 };
